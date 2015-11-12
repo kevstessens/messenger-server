3fb114f22b7168852edc001a9ae9223ef9a24669
Kevstessens <Kevstessens@gmail.com>
2015-11-12 13:47:00 -0300

    more

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :add_allow_credentials_headers
 
   def add_allow_credentials_headers
     response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
@@ -12,5 +13,5 @@ class ApplicationController < ActionController::Base
   def options
     head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type, authorization, User-App-Token'
   end
-  
+
 end
